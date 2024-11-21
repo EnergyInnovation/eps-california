0a8a64ff97972e2b98d1f3f3a6e12f294b8ae6c8
oashmoore <66978250+oashmoore@users.noreply.github.com>
2024-11-20 16:10:31 -0800

    4.0.3 model and data

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -4,7 +4,7 @@ SIMULATE>SAVELIST|OutputVarsToExport.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2020|2050|:NoSettings
+MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2021|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
 
 SIMULATE>SAVELIST|
