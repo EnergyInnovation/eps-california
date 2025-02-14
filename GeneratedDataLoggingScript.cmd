06fd7b9469832bb4959e3bf38de2d654abf80ca5
oashmoore <66978250+oashmoore@users.noreply.github.com>
2025-02-13 18:07:01 -0800

    4.0.3 files

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -1,10 +1,10 @@
 SPECIAL>LOADMODEL|"EPS.mdl"
 
-SIMULATE>SAVELIST|OutputVarsToExport.lst
+SIMULATE>SAVELIST|OutputVarsToExport webapp graphs.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2021|2050|:NoSettings
+MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport webapp graphs.lst|+!||2021|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
 
 SIMULATE>SAVELIST|
